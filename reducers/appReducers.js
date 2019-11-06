bbe25539a71bab6e4b1a268fad56f967306a581d
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-11-06 16:48:23 -0600

    Add reducers to handle token and athlete data

--- reducers/appReducers.js
+++ reducers/appReducers.js
@@ -1,13 +1,16 @@
+import utilities from '../utils/utilities';
+
 const appReducers = ( state, action ) => {
     switch( action.type ){
         case 'handleLoading':
             return {...state, isLoading: action.payload.status }
         case 'updateMounted':
             return {...state, isMounted: action.payload.status }
-        case 'updateAtheleteData':
+        case 'updateAthleteData':
+            console.log( 'Update Athlete DAta', action.payload.athleteData );
             return { ...state, athleteData: action.payload.athleteData }
         case 'storeToken':
-                localStorage.setItem('token', action.payload.token );
+                utilities.setLocalToken( action.payload.token );
                 return { ...state, token: action.payload.token }
         case 'handleAuthorized':
                 return { ...state, isAuth: action.payload.status }
