8eb14a98874325f770357034e835eab12aad36d6
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-11-06 16:49:32 -0600

    Home layout

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,59 @@
 import React ,{ useEffect, useContext, useState} from 'react';
+import {
+    Grid,
+    Tabs,
+    Tab,
+    Box,
+    AppBar,
+    Typography,
+} from '@material-ui/core';
+
+const a11yProps = (index) => {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  };
+
+  const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
 
 const Home = () => {
+    const [value, setValue] = useState(null);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+      };
     return (
-        <div>
-            Home
-        </div>
+        <Grid container >
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                <Tab label="Item One" {...a11yProps(0)} />
+                <Tab label="Item Two" {...a11yProps(1)} />
+                <Tab label="Item Three" {...a11yProps(2)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+        </Grid>
     )
 }
 export default Home;
