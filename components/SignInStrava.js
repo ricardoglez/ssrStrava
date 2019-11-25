9b45747a0b53295884e1a6701ab782631aeb4b6e
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-11-25 17:56:10 -0600

    handle login url

--- components/SignInStrava.js
+++ components/SignInStrava.js
@@ -6,12 +6,12 @@ import {baseStyles} from '../themes/customStyles';
 
 const SignInStrava = () => {
     const classes = baseStyles(); 
-
+    const loginUrl = `https://www.strava.com/oauth/authorize?client_id=40487&scope=read_all,activity:read_all&response_type=code&redirect_uri=http://localhost:3000&approval_prompt=force`
     return (
         <Grid container direction='column' justify='center'>
             <Typography className={classes.textCenter}> Identifícate </Typography>
             <Button variant='contained' color='secondary'>
-                <a className={classes.linkWithoutStyle} href='https://www.strava.com/oauth/authorize?client_id=40487&response_type=code&redirect_uri=http://localhost:3000&approval_prompt=force'>
+                <a className={classes.linkWithoutStyle} href={loginUrl}>
                 Entra con tu cuenta de Strava
                 </a>
             </Button>
